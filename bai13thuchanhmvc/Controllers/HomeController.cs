0f794e3892b1784963f6d87905b279e33c7a1358
phungphat68 <phatph297@gmail.com>
2021-07-08 17:33:02 +0700

    Add project files.

---
+++ bai13thuchanhmvc/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bai13thuchanhmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from Home controller";
+        }
+    }
+}
